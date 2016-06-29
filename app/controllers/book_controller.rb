28cc0c05c69502f021e62d497eca7cb716d7ab90
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-06-29 00:15:24 -0500

    generated controllers and model for book

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,25 @@
+class BookController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
